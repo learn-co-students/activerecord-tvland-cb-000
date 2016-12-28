6535481fdbc3dab5ff28f8e550828664a40c469d
Derek Kaknes <derek.kaknes@gmail.com>
2016-12-28 18:46:49 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,12 @@
 class Character < ActiveRecord::Base
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{self} always says: #{self.catchphrase}"
+  end
   
+  def to_s
+    self.name
+  end
 end
