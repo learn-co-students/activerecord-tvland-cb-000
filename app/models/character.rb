8fb15f56521f4b061f689dc797195cab79587c31
David Slivken <slivken@gmail.com>
2017-02-06 12:33:49 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,21 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  def build_network (network)
+    n = Network.create(network)
+    n.shows << self.show
+    self
+  end
+
+  def build_show (show)
+    s = Show.create(show)
+    s.characters << self
+    self
+  end
+
 end
