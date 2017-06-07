6f893f52fe2a29c75ac1e7fa12d40cb4726c8169
James Garcia <jgarcia@garciajames.com>
2017-06-07 16:26:34 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def build_network(call_letters)
+    network = Network.new(call_letters)
+    network.shows << self
+    network
+  end
 end
