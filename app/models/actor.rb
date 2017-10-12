d2412c58025d4a41074018fb1edf81d88376aa55
sjmoody <steven@beachheadmarketing.com>
2017-10-12 13:30:58 +0200

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,26 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  #Associate the Actor model with the Character and Show model.
+  #An actor should have many characters and many shows through characters.
+  has_many :characters
+  has_many :shows, through: :characters
+
+  # TODO create an instance method on actor called full_name to return first and
+  #last name together
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+
+
+  # TODO create a list_roles method
+  # TODO: build a method on actor that will return an array of
+  # strings in the form "#{character_name} - #{show_name}"
+  def list_roles
+    roles = []
+    self.characters.each do |c|
+      roles << c.name + " - " + c.show.name
+    end
+
+    roles
+  end
 end
