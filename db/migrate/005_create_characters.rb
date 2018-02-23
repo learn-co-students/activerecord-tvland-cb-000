906b1a1d3a9ac304a60495021f779227d24744d3
Jen Poole <contact.vandepool@gmail.com>
2018-02-23 04:19:29 +0000

    Done.

---
+++ db/migrate/005_create_characters.rb
@@ -0,0 +1,9 @@
+class CreateCharacters < ActiveRecord::Migration[4.2]
+  def change
+    create_table :characters do |t|
+      # A character should have a name and a show_id
+      t.string :name
+      t.integer :show_id
+    end
+  end
+end
