abbf5db4123d0f510c7c28426b3810cf5bb3ac25
Jen Poole <contact.vandepool@gmail.com>
2018-02-23 04:25:21 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,4 +2,9 @@ class Actor < ActiveRecord::Base
   # An actor should have many characters and many shows through characters
   has_many :characters
   has_many :shows, through: :characters
+
+  # Write a method in the Actor class, #full_name, that returns the first and last name of an actor
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
