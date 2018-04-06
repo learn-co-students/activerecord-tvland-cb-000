04b2ea5fcc3c66a5e63135f4011d07af75cc5420
Jawann <jawann@gmail.com>
2018-04-06 03:28:52 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    holder = []
+    self.characters.find_each {|char| holder.push("#{char.name} - #{char.show.name}")}
+    holder
+  end
 end
