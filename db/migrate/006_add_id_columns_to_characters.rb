b7e9f9e759ac53fde5a5f52e2d387a7b7d2c2526
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-10-10 05:11:18 +0000

    Done.

---
+++ db/migrate/006_add_id_columns_to_characters.rb
@@ -0,0 +1,6 @@
+class AddIdColumnsToCharacters < ActiveRecord::Migration[5.1]
+  def change
+    add_column :characters, :actor_id, :integer
+    add_column :characters, :show_id, :integer
+  end
+end
