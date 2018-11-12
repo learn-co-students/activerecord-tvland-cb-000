3e1e936d7e380e96cd71578412a9c4fbce83d6fb
Sergio Marrocoli <s.marrocoli@gmail.com>
2018-11-12 17:33:00 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,8 +3,8 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   belongs_to :network
 
-  def build_network(call_letters:)
-    self.network = Network.create(:call_letters => call_letters)
-  end
+  # def build_network(call_letters:)
+  #   self.network = Network.create(:call_letters => call_letters)
+  # end
 
 end
