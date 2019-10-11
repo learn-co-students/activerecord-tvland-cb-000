36ed2c1971e18779e4e416868f183752989c3c34
Will Hodgson <will.hodgson@current-rms.com>
2019-10-11 09:28:04 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    actors_to_return = []
+    self.actors.all.each do |actor|
+      actors_to_return << actor.full_name
+    end
+    actors_to_return
+  end
 end
